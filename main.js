f525ca7dac683bc75d33272981c373e24cfdd887
jharte5 <jonathan.harte@codeimmersives.com>
2019-10-17 15:12:20 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-function getFirstItemFrom() {
-
+function getFirstItemFrom(arr) {
+  return arr[0]
 }
 
-function getLastItemFrom() {
-
+function getLastItemFrom(arr) {
+  const lastIndex = arr.length - 1
+  return arr[lastIndex]
 }
 
-function getIndex3() {
-
+function getIndex3(arr) {
+  // return   arr.splice(5, 2) && arr[3];
+  return arr[3];
+  
+  
 }
 
 function firstItemIsNumber() {
-
+  return 
 }
 
 function isLongList() {
